88baad958b1ec8044f31229532312d90264b7546
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2023-03-09 22:08:10 +0530

    tesseract removed

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
 #tesseract our ML model
-import pytesseract as tess
+# import pytesseract as tess
 
 
 
@@ -27,26 +27,26 @@ def home():
     return "API is working fine"
 
 
-#Creating an route that handels our POST request from the frontend i.e image is received here {http://127.0.0.1:7777/upload-image} is our POST request path
-@app.route('/upload-image', methods=['POST'])
-#calling CORS
-@cross_origin()
+# #Creating an route that handels our POST request from the frontend i.e image is received here {http://127.0.0.1:7777/upload-image} is our POST request path
+# @app.route('/upload-image', methods=['POST'])
+# #calling CORS
+# @cross_origin()
 
-#function that handles our image operations
-def upload_image():
+# #function that handles our image operations
+# def upload_image():
     
-    # Get the image data from the POST request
-    image_data = request.files.get('image').read()
+#     # Get the image data from the POST request
+#     image_data = request.files.get('image').read()
 
     
-   # Use PIL to load the image from memory
-    image = Image.open(io.BytesIO(image_data))
+#    # Use PIL to load the image from memory
+#     image = Image.open(io.BytesIO(image_data))
 
-    # Use Tesseract to extract text from the image
-    text=tess.image_to_string(image)
+#     # Use Tesseract to extract text from the image
+#     text=tess.image_to_string(image)
 
-    # Send a response back to the client
-    return text
+#     # Send a response back to the client
+#     return text
 
 #Assigning the port 7777 to the python api
 if __name__ == '__main__':
